b7cc695a90cdaac745071824f3053e800d5dd362
Kathryn Janzen <kathryn.janzen@lightsource.ca>
2010-11-09 10:23:26 -0600

    modified:   imm/lims/views.py

--- imm/lims/views.py
+++ imm/lims/views.py
@@ -988,8 +988,10 @@ def shipment_pdf(request, id):
                         env={'TEXINPUTS' : '.:' + settings.TEX_TOOLS_DIR + ':',
                              'PATH' : settings.TEX_BIN_PATH},
                         cwd=temp_dir,
-                        stdout=stdout.fileno(),
-                        stderr=stderr.fileno(),
+                        #stdout=stdout.fileno(),
+                        #stderr=stderr.fileno(),
+                        stdout = devnull,
+                        stderr = devnull,
                         stdin=devnull
                         )
         
