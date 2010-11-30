a81812bf1829c758fd3a3970dade2385f798e8a7
Kathryn Janzen <kathryn.janzen@lightsource.ca>
2010-11-30 10:40:27 -0600

    pdf tex change

--- imm/settings_local.py
+++ imm/settings_local.py
@@ -15,7 +15,7 @@ LDAP_FULL_NAME = 'cn'
 LDAP_BINDDN = 'ou=people,dc=cmcf,dc=cls'
 LDAP_BIND_ATTRIBUTE = 'uid'
 
-TEXINPUTS = ':/var/website/lims-website/imm/tex//'
+#TEXINPUTS = ':/var/website/lims-website/imm/tex//'
 USER_API_HOST = 'localhost:10001'
 #DOWNLOAD_FRONTEND = 'django'
 DOWNLOAD_CACHE_DIR = '/webcache/cache'
