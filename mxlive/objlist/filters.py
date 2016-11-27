2a7c0f19d86a162e6669c0a23979944063dde841
Michel Fodje <michel.fodje@lightsource.ca>
2016-11-27 02:42:37 -0600

    Updated Weekly date filter

--- mxlive/objlist/filters.py
+++ mxlive/objlist/filters.py
@@ -9,7 +9,7 @@ def get_week_extent(dt):
     a tuple"""
     year, week, day = dt.isocalendar()
     sd = dt - timedelta(days=day-1)
-    ed = sd + timedelta(days=6)
+    ed = sd + timedelta(days=7)
     return (sd, ed)
 
 def get_specs_for_date(date_str):
@@ -36,7 +36,7 @@ class WeeklyDateFilter(SimpleListFilter):
         prev_week, cur_week, next_week = get_specs_for_date(request.GET.get('weekof', None))
         return (
             (prev_week[0].strftime('%Y-%m-%d'), "%s %s" % (_('Week of'), dateformat.format(prev_week[0], 'M jS'))),
-            (prev_week[0].strftime('%Y-%m-%d'), "%s %s" % (_('Week of'), dateformat.format(cur_week[0], 'M jS'))),
+            (cur_week[0].strftime('%Y-%m-%d'), "%s %s" % (_('Week of'), dateformat.format(cur_week[0], 'M jS'))),
             (next_week[0].strftime('%Y-%m-%d'), "%s %s" % (_('Week of'), dateformat.format(next_week[0], 'M jS'))),
         )
     
@@ -46,7 +46,7 @@ class WeeklyDateFilter(SimpleListFilter):
             dt = timezone.now().date()
         week_start, week_end = get_week_extent(dt)
         qs_str = {'%s__gte' % self.field: week_start,
-                  '%s__lte' % self.field: week_end}
+                  '%s__lt' % self.field: week_end}
         return queryset.filter(**qs_str)
     
     
