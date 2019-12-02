9664ac91243db3298d57b19acd5ff49bf664f482
Michel Fodje <michel.fodje@lightsource.ca>
2019-12-02 16:21:52 -0600

    Add migrations

--- mxlive/settings.py
+++ mxlive/settings.py
@@ -213,6 +213,22 @@ DOWNLOAD_PROXY_URL = "http://mxlive-data/download"
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
+DEBUG_TOOLBAR_PANELS = [
+    'ddt_request_history.panels.request_history.RequestHistoryPanel',  # Here it is
+    'debug_toolbar.panels.versions.VersionsPanel',
+    'debug_toolbar.panels.timer.TimerPanel',
+    'debug_toolbar.panels.settings.SettingsPanel',
+    'debug_toolbar.panels.headers.HeadersPanel',
+    'debug_toolbar.panels.request.RequestPanel',
+    'debug_toolbar.panels.sql.SQLPanel',
+    'debug_toolbar.panels.templates.TemplatesPanel',
+    'debug_toolbar.panels.staticfiles.StaticFilesPanel',
+    'debug_toolbar.panels.cache.CachePanel',
+    'debug_toolbar.panels.signals.SignalsPanel',
+    'debug_toolbar.panels.logging.LoggingPanel',
+    'debug_toolbar.panels.redirects.RedirectsPanel',
+    'debug_toolbar.panels.profiling.ProfilingPanel',
+]
 
 try:
     from local.settings import *
