18bd233a5222963cab0e04772cadfa01931c5b63
Michel 4J <michel4j@gmail.com>
2019-12-03 07:56:38 -0600

    Optimization ...

--- mxlive/settings.py
+++ mxlive/settings.py
@@ -51,6 +51,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'django.contrib.humanize',
     'debug_toolbar',
+    'memoize',
     'itemlist',
     'mxlive.staff',
     'mxlive.lims',
