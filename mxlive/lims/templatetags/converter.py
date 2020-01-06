ff677515d1bc7605c00e52ba5a90276e3211808c
Michel 4J <michel4j@gmail.com>
2020-01-05 22:26:36 -0600

    Fix energy conversion in diffviewer

--- mxlive/lims/templatetags/converter.py
+++ mxlive/lims/templatetags/converter.py
@@ -11,6 +11,7 @@ _c = 299792458e10   # A/s
 @register.filter("energy_to_wavelength")  
 def energy_to_wavelength(energy): 
     """Convert energy in keV to wavelength in angstroms."""
+    energy = float(energy)
     if energy == 0.0:
         return 0.0
     return (_h*_c)/(energy*1000.0)
