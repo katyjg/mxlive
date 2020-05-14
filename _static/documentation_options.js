d2691dd9b4fed9b1e27b7420a6803bac810fe004
Kathryn Janzen <kathryn@localhost-live.clsi.ca>
2020-05-14 11:08:38 -0600

    "Documentation rebuilt"

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: 'v2019.04-235-g9a2b884',
+    VERSION: 'v2019.04-238-g93a9afb',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
