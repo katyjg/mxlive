b58ec2657fe6db574ae016c38ce0b852bb70301c
Michel Fodje <michel4j@gmail.com>
2026-02-26 09:14:29 -0600

    Fix regex for sample name validation in forms.py; ensure names do not contain spaces or special characters

--- mxlive/lims/forms.py
+++ mxlive/lims/forms.py
@@ -280,7 +280,7 @@ class SampleForm(forms.ModelForm):
         if 'name' in self.cleaned_data:
             if self.instance.group.samples.exclude(pk=self.instance.pk).filter(name=self.cleaned_data['name']).exists():
                 self._errors['name'] = self.error_class(['Each sample in the group must have a unique name'])
-            if not re.compile('^[a-zA-Z0-9-_]+[\w]+$').match(self.cleaned_data['name']):
+            if not re.compile(r'^[a-zA-Z0-9-_]+\w+$').match(self.cleaned_data['name']):
                 self._errors['name'] = self.error_class(['Name cannot contain any spaces or special characters'])
         return self.cleaned_data
 
